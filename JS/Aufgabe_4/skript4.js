e0f1add98c2f9611f672553797bd3e20b7d7690b
Magnus <magnus.rachbauer@student.htldornbirn.at>
2024-01-11 11:04:27 +0100

    Task 4 /finished

---
+++ JS/Aufgabe_4/skript4.js
@@ -0,0 +1,50 @@
+console.log("test"); 
+
+//for (let count = 0; count < 10; count++) {
+    //console.log("Magnus");
+//}
+//for (let count = 10; count >= 0; count--) {
+   // console.log(count + " Andreas")
+// }
+
+
+
+//Jetzt Übung
+// von 7 bis 45 Zählen
+for (let i = 7; i <= 45; i++) {
+    console.log(i);
+  }
+  
+  //von 9 bis 99 Zählen
+  for (let i = 9; i <= 99; i++) {
+    console.log(i);
+  }
+  
+  // rückwärts von 10 bis 0 Zälhen 
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+  
+  // rückwärts Zählen von 1234 bis 1207
+  for (let i = 1234; i >= 1207; i--) {
+    console.log(i);
+  }
+  
+  // Meinen namen geb ich genau 8 mal aus
+  for (let i = 0; i < 8; i++) {
+    console.log("Magnus");
+  }
+  
+  // von 0 auf 10, 
+  for (let i = 0; i <= 10; i++) {
+    console.log(i);
+  }
+  
+  console.log("Magnus");
+  
+  for (let i = 10; i >= 0; i--) {
+    console.log(i);
+  }
+  
+console.log("Fertig"); 
+
