26b7de9c16b6d28c84433b0cbc213b77c108d3be
Magnus <magnus.rachbauer@student.htldornbirn.at>
2024-01-11 11:08:31 +0100

    Task5/stardet

--- skript5.js
+++ skript5.js
@@ -1,8 +1,32 @@
-let number = 27;
-if (number < 25) {
-   console.log(number);
-} else if (number < 50) {
-   console.log("kleiner 50 " + number)
+//let number = 27;
+//if (number < 25) {
+ //  console.log(number);
+//} else if (number < 50) {
+//   console.log("kleiner 50 " + number)
+//} else {
+ //  console.log("else");
+//}
+
+
+//jedtztÜbung
+
+let number1 = 35; 
+
+if (number1 < 20) {
+  console.log("Mini");
+} else if (number1 >= 20 && number1 <= 50) {
+  console.log("Medium");
 } else {
-   console.log("else");
+  console.log("Large");
+}
+
+
+let number2 = 75; 
+
+if (number2 <= 7) {
+  console.log("Mini");
+} else if (number2 > 7 && number2 <= 90) {
+  console.log("Medium");
+} else {
+  console.log("Large");
 }
